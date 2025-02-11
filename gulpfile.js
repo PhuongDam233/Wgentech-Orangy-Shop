3e36e0e925032a8c5f0233ce2016b0bdf1c9bd4e
PhuongDam233 <phuongdamthu233@gmail.com>
2025-02-11 10:41:28 +0700

    Intital project

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+function buildStyles() {
+  return gulp
+    .src("sass/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./assets/"));
+}
+
+exports.buildStyles = buildStyles;
+
+exports.watch = function () {
+  gulp.watch("./sass/**/*.scss", build);
+};
